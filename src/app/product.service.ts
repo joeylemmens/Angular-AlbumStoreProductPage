5bb90ff0a9fa780a311b85d6d21899dcb143eab2
Joey Lemmens <joeylemmens@gmail.com>
2018-02-04 14:52:17 +0100

    part3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,12 +8,10 @@ export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
 
-  constructor(private _http: Http) {
+  constructor(private _http: Http) { }
 
-  }
-
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => {
+  getAlbum(id:number) {
+    return this._http.get(this._albumUrl).map(response => {
       response.json();
     });
   }
